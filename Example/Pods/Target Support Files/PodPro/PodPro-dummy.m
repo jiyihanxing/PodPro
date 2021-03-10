07439ec8eb07b3ba6a1bb0c15e3b5ecfa9a27171
Tc_fenghx <Tc_fenghx@yiche.com>
2021-03-10 18:46:49 +0800

    ...

--- Example/Pods/Target Support Files/PodPro/PodPro-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PodPro : NSObject
-@end
-@implementation PodsDummy_PodPro
-@end
--- Example/Pods/Target Support Files/Pods-PodPro_Example/Pods-PodPro_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PodPro_Example : NSObject
-@end
-@implementation PodsDummy_Pods_PodPro_Example
-@end
